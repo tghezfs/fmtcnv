82117f693beadcf721fde23ee7915e7bc59a7c4d
tghezfs <tghez99@gmail.com>
2026-04-16 00:55:58 -0600

    Added clap, some validations, supported formats and some fs ops

---
+++ src/cli.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long)]
+    pub file: String,
+
+    #[arg(short, long)]
+    pub to_format: String
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+use std::io::{Error as IOError, ErrorKind};
+use std::path::Path;
+
+mod cli;
+use clap::Parser;
+use cli::Args;
+
+const VALID_FORMATS: [&str; 4] = ["json", "yaml", "yml", "toml"]; 
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    println!("args = {:?}", args);
+    let out_format = parse_format(&args.to_format)?;
+
+    let path = Path::new(&args.file);
+
+    if path.metadata()?.is_dir() {
+        return Err( Box::new(IOError::new(ErrorKind::IsADirectory, "Cannot parse or read a dir")) );
+    }
+
+    let abs_path = fs::canonicalize(path)?;
+
+    match abs_path.extension() {
+        Some(ext) => {
+            let extension = ext
+                .to_string_lossy()
+                .to_string()
+                .to_lowercase();
+
+            if VALID_FORMATS.contains(&extension.as_str()) {
+                let _content = fs::read_to_string(abs_path)?;
+
+                let in_format = parse_format(&extension)
+                    .expect("Input Format must be valid at this point!");
+
+                match in_format {
+                    Format::Json => {},
+                    Format::Yaml => {},
+                    Format::Toml => {}
+                }
+
+                match out_format {
+                    Format::Json => {},
+                    Format::Yaml => {},
+                    Format::Toml => {}
+                }
+            } else {
+                todo!()
+            }
+        },
+        None => { todo!() }
+    }
+    Ok(())
+
+}
+
+enum Format {
+    Json,
+    Yaml,
+    Toml
+}
+
+fn parse_format(format: &str) -> Result<Format, Box<dyn Error>> {
+    match format.to_lowercase().as_str() {
+        "json" => Ok(Format::Json),
+        "yaml" | "yml" => Ok(Format::Yaml),
+        "toml" => Ok(Format::Toml),
+        _ => Err(Box::new(IOError::new(
+                    ErrorKind::InvalidInput, "Invalid Format!")))
+    }
 }
